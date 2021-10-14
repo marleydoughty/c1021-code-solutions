aaaab203e77a0d708ef0369fa5577c5be94c3f32
Marley Doughty <marleydoughty@yahoo.com>
2021-10-13 23:09:24 -0700

    working on javascript-loops

---
+++ javascript-loops/main.js
@@ -0,0 +1,33 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+function getNumbersToTen() {
+  var numbers = [];
+  var currentNumber = 1;
+  while (currentNumber <= 10) {
+    numbers.push(currentNumber++);
+  }
+  return numbers;
+}
+function getEvenNumbersToTwenty() {
+  var evenNumbers = [];
+  var currentNumber = 2;
+  while (currentNumber <= 20) {
+    evenNumbers.push(currentNumber += 2);
+  }
+  return evenNumbers;
+}
+// function repeatWord(word, times) {
+//   var count = 1;
+//   var repeated = '';
+// }
+
+function logEachCharacter(string) {
+  for (var i = 0; i < string.length; i++);
+  console.log(string[i]);
+}
