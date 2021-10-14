ca2fa49719c4186e7459d4308537532587c50529
Marley Doughty <marleydoughty@yahoo.com>
2021-10-14 15:38:22 -0700

    completed javascript-loops

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -10,7 +10,8 @@ function getNumbersToTen() {
   var numbers = [];
   var currentNumber = 1;
   while (currentNumber <= 10) {
-    numbers.push(currentNumber++);
+    numbers.push(currentNumber);
+    currentNumber++;
   }
   return numbers;
 }
@@ -18,16 +19,47 @@ function getEvenNumbersToTwenty() {
   var evenNumbers = [];
   var currentNumber = 2;
   while (currentNumber <= 20) {
-    evenNumbers.push(currentNumber += 2);
+    evenNumbers.push(currentNumber);
+    currentNumber += 2;
   }
   return evenNumbers;
 }
-// function repeatWord(word, times) {
-//   var count = 1;
-//   var repeated = '';
-// }
+function repeatWord(word, times) {
+  var count = 1;
+  var repeated = '';
+  while (count <= times) {
+    repeated += word;
+    count++;
+  }
+  return repeated;
+}
 
 function logEachCharacter(string) {
-  for (var i = 0; i < string.length; i++);
-  console.log(string[i]);
+  for (var i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
+
+function doubleAll(numbers) {
+  var doubled = [];
+  for (var i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+  return doubled;
+}
+
+function getKeys(object) {
+  var keys = [];
+  for (var key in object) {
+    keys.push(key);
+  }
+  return keys;
+}
+
+function getValues(object) {
+  var values = [];
+  for (var key in object) {
+    values.push(object[key]);
+  }
+  return values;
 }
