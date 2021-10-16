a61fd03f64caaec957cf55f9d10b46fe14d92d2d
Marley Doughty <marleydoughty@yahoo.com>
2021-10-16 14:36:07 -0700

    working on string-manipulation-1

--- string-manipulation-1/src/get-first-char.js
+++ string-manipulation-1/src/get-first-char.js
@@ -1,17 +1,9 @@
 /* exported getFirstChar */
 
 // create storage space for output (empty string);
-// look at each character in the string starting with the first character;
 // once reached the first character/first index, assign that value into the output;
 // return value of the output;
 
-// function getFirstChar(string) {
-//   var output = '';
-//   for (var i = 0; i < string.length; i++) {
-//     output = string[0];
-//   }
-//   return output;
-// }
 function getFirstChar(string) {
   var output = '';
   output = string[0];
