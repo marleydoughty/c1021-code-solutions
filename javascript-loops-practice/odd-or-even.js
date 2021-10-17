7224b94ef749147342e9a93aa0961f46f66ec80b
Marley Doughty <marleydoughty@yahoo.com>
2021-10-17 10:34:07 -0700

    completed javascript-loops-practice

--- javascript-loops-practice/odd-or-even.js
+++ javascript-loops-practice/odd-or-even.js
@@ -1,4 +1,11 @@
 /* exported oddOrEven */
 function oddOrEven(numbers) {
-
+  var output = [];
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      output.push('even');
+    } else {
+      output.push('odd');
+    }
+  } return output;
 }
