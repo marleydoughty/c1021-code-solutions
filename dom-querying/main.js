72ed6990ffc435ec76266b800a0f17beba4ebf43
Marley Doughty <marleydoughty@yahoo.com>
2021-10-18 14:40:32 -0700

    completed dom-querying

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,1 +1,9 @@
 console.log('hello, world!');
+document.querySelector('h1');
+console.dir(document.querySelector('h1'));
+document.querySelector('#explanation');
+console.dir(document.querySelector('#explanation'));
+document.querySelector('.hint');
+console.dir(document.querySelector('.hint'));
+document.querySelectorAll('p');
+document.querySelectorAll('#example-links');
