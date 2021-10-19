89172c24c141d2d5b5da076a3a9387fc679ed13b
Marley Doughty <marleydoughty@yahoo.com>
2021-10-19 09:52:43 -0700

    completed dom-manipulation

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -2,7 +2,7 @@
 create variable for class click-count;
  increment the button click by one
  assign counter clicks to the click-count variable
- */
+ create conditions for number of clicks and color coordination */
 
 var $hotButton = document.querySelector('.hot-button');
 var $clickCount = document.querySelector('.click-count');
@@ -13,13 +13,13 @@ function incrementClick() {
   counter++;
   if (counter < 4) {
     $hotButton.className = 'hot-button cold';
-  } else if (counter >= 4 || counter < 7) {
+  } else if (counter >= 4 && counter < 7) {
     $hotButton.className = 'hot-button cool';
-  } else if (counter >= 7 || counter < 10) {
+  } else if (counter >= 7 && counter < 10) {
     $hotButton.className = 'hot-button tepid';
-  } else if (counter >= 10 || counter < 13) {
+  } else if (counter >= 10 && counter < 13) {
     $hotButton.className = 'hot-button warm';
-  } else if (counter >= 13 || counter < 16) {
+  } else if (counter >= 13 && counter < 16) {
     $hotButton.className = 'hot-button hot';
   } else {
     $hotButton.className = 'hot-button nuclear';
