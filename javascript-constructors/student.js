be0ce04d2718ad0d91a02fb8fa93a0bde91dc293
Marley Doughty <marleydoughty@yahoo.com>
2021-11-01 21:11:34 -0700

    completed javascript-constructors

--- javascript-constructors/student.js
+++ javascript-constructors/student.js
@@ -3,26 +3,10 @@ function Student(firstName, lastName, subject) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.subject = subject;
-  this.getFullName = function () {
-    return this.firstName + ' ' + this.lastName;
-  };
-  this.getIntroduction = function () {
-    return 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
-  };
 }
-
-var newStudent = new Student('marley', 'doughty', 'web dev');
-// console.log(newStudent);
-
-var getFullName = {
-  fullName: function () {
-    return this.firstName + ' ' + this.lastName;
-  }
+Student.prototype.getFullName = function () {
+  return this.firstName + ' ' + this.lastName;
 };
-var getIntroduction = {
-  intro: function () {
-    return 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
-  }
+Student.prototype.getIntroduction = function () {
+  return 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
 };
-Object.setPrototypeOf(newStudent, getFullName);
-Object.setPrototypeOf(newStudent, getIntroduction);
