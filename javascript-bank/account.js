6f014504db583963f1a2d3d5c7e5498492584b8e
Marley Doughty <marleydoughty@yahoo.com>
2021-11-03 22:31:47 -0700

    workin on javascript-bank

---
+++ javascript-bank/account.js
@@ -0,0 +1,15 @@
+/* exported Account */
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+Account.prototype.deposit = function () {
+
+};
+Account.prototype.withdraw = function () {
+
+};
+Account.prototype.getBalance = function () {
+
+};
