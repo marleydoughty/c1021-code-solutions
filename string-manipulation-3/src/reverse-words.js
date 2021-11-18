19698e4f7a0aa43b2924b64797d618bce2633916
Marley Doughty <marleydoughty@yahoo.com>
2021-11-18 14:58:07 -0800

    working on level 3 algorithms

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -6,10 +6,6 @@
  * return the output
  */
 function reverseWords(string) {
-  var output = '';
-  var newStr = string.split(' ');
-  for (var i = newStr[newStr.length - 1]; i > 0; i--) {
-    output += (newStr[i]);
-  }
-  return output;
+  // var output = [];
+
 }
