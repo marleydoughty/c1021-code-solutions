e96adbe7aa47b67951a72cd8da89f84c2c11598e
Marley Doughty <marleydoughty@yahoo.com>
2021-11-17 20:26:44 -0800

    working on string-manipulation-3

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,7 @@
+/* exported isPalindromic */
+/**
+ * look at each index in the string
+ * take the last index of each string and put add it to a new string
+ * compare new string and original string
+ * if === then return true, else return false
+ */
