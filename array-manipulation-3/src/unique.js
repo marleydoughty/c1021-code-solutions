041e85398844976ffac26bc212c9af51442aeaea
Marley Doughty <marleydoughty@yahoo.com>
2021-11-19 11:15:30 -0800

    working on level 3 algorithms

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,7 @@
 /* exported unique */
+function unique(array) {
+  var newArray = array.filter(function (item, i, array) {
+    return array.indexOf(item) === i;
+  });
+  return newArray;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,7 @@
 /* exported zip */
+function zip(first, second) {
+  var newArray = first.map(function (e, i) {
+    return [e, second[i]];
+  });
+  return newArray;
+}
