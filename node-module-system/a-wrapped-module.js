b57385eed3b4d7292cf17e04090ae242a01a4afd
Marley Doughty <marleydoughty@yahoo.com>
2021-11-24 20:23:36 +0000

    completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,10 @@
+console.log('typeof exports:', typeof exports);
+console.log('value of exports:', exports);
+console.log('typeof require:', typeof require);
+console.log('value of require:', require);
+console.log('typeof module:', typeof module);
+console.log('value module:', module);
+console.log('typeof dirname:', typeof __dirname);
+console.log('value dirname:', __dirname);
+console.log('typeof filename:', typeof __filename);
+console.log('value filename:', __filename);
