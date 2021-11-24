2c943a7de2f4694703793be54a8a875e1eceeaae
Marley Doughty <marleydoughty@yahoo.com>
2021-11-24 05:06:21 +0000

    completed node-process-argv

--- node-process-argv/echo.js
+++ node-process-argv/echo.js
@@ -1,17 +1,1 @@
-const echo1 = 'Command line arguments!';
-const echo2 = 'Hello, computer!';
-const echo3 = 'No, YOU are a computer.';
-const echo4 = 'STOP THAT';
-
-if (echo1) {
-  console.log('Command line arguments!');
-}
-if (echo2) {
-  console.log('Hello, computer!');
-}
-if (echo3) {
-  console.log('No, YOU are a computer.');
-}
-if (echo4) {
-  console.log('STOP THAT');
-}
+console.log(process.argv[2]);
