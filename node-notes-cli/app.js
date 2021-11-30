006b6d874f7d94de42772a7e5d369fd54d2658a5
Marley Doughty <marleydoughty@yahoo.com>
2021-11-30 19:35:14 +0000

    completed update command

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -4,16 +4,26 @@ const fileObj = require('./data.json');
 const args = process.argv;
 const firstInput = args[2];
 const secondInput = args[3];
-// const thirdInput = args[4];
+const thirdInput = args[4];
 
 const readCommand = () => {
   for (const [key, value] of Object.entries(fileObj.notes)) {
     console.log(`${key}: ${value}`);
   }
 };
+
 const createCommand = () => {
   fileObj.notes[fileObj.nextId] = secondInput;
   fileObj.nextId++;
+  fs.writeFile('./data.json', JSON.stringify(fileObj, null, 2) + '\n', err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
+
+const updateCommand = () => {
+  fileObj.notes[secondInput] = thirdInput;
   fs.writeFile('./data.json', JSON.stringify(fileObj, null, 2) + '\n', err => {
     if (err) {
       console.log(err);
@@ -25,8 +35,8 @@ if (firstInput === 'read') {
   readCommand();
 } else if (firstInput === 'create') {
   createCommand();
-// } else if (firstInput === 'update') {
-//   updateCommand();
+} else if (firstInput === 'update') {
+  updateCommand();
 // } else if (firstInput === 'delete') {
 //   deleteCommand();
 // } else {
