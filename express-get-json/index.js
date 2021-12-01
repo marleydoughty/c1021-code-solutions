4f70fcfe5307b5454a954122c0f30efc6fdd7332
Marley Doughty <marleydoughty@yahoo.com>
2021-12-01 02:01:11 +0000

    working on turning objects into array of objects

---
+++ express-get-json/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+  res.send('Got a grades API request');
+});
+
+app.listen(3000, () => {
+  // console.log('server is listening on port 3000');
+});
