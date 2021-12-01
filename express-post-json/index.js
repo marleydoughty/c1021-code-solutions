d7a4f7e5856ec9364d96b9acc9e50b0ecb3c7417
Marley Doughty <marleydoughty@yahoo.com>
2021-12-01 23:50:28 +0000

    completed express-post-json

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const nextID = 1;
+let nextID = 1;
 const grades = {};
 express.json();
 
@@ -14,11 +14,17 @@ app.get('/api/grades', (req, res) => {
 
 app.use(express.json());
 
-app.post('/api/grades', (req, res, next) => {
-
-});
-
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Server is listening on port 3000');
+});
+
+app.post('/api/grades', (req, res) => {
+  const body = req.body;
+  body.id = nextID;
+  nextID++;
+  grades[body.id] = body;
+
+  res.status(201);
+  res.json(grades[body.id]);
 });
