89c46b277f83df94be47b475748b4c8e1a72c7d3
Marley Doughty <marleydoughty@yahoo.com>
2021-12-03 07:55:56 +0000

    completed title-case

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,15 +1,42 @@
 /* exported titleCase */
-const lowLetters = ['or', 'the', 'a', 'an', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'and', 'to', 'nor', 'but'];
-const knownCases = ['JavaScript', 'API'];
 function titleCase(title) {
-  return title.split(' ').map((word, ii, arr) => {
-    if (lowLetters.includes(word)) {
-      return word;
+  var output = [];
+  var lowLetters = ['or', 'the', 'a', 'an', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'and', 'to', 'nor', 'but'];
+  var titleArray = [];
+  var hyphenArray = [];
+  var words = title.toLowerCase().split(' ');
+
+  for (var i = 0; i < words.length; i++) {
+    if (i === 0 || words[i - 1][words[i - 1].length - 1] === ':') {
+      titleArray.push(words[i][0].toUpperCase() + words[i].slice(1));
+    } else if (lowLetters.includes(words[i])) {
+      titleArray.push(words[i]);
+    } else if (words[i].toLowerCase() === 'javascript') {
+      titleArray.push('JavaScript');
+    } else if (words[i].toLowerCase() === 'api') {
+      titleArray.push('API');
+    } else if (i > 0) {
+      titleArray.push(words[i][0].toUpperCase() + words[i].slice(1));
     }
-    const foundKnownWord = knownCases.find(knownWord => knownWord.toLowerCase() === word.toLowerCase());
-    if (foundKnownWord) {
-      return foundKnownWord;
+  }
+  for (var x = 0; x < titleArray.length; x++) {
+    if (titleArray[x].includes('-')) {
+      var includesHyphen = titleArray[x].split('-');
+      includesHyphen = includesHyphen[0][0].toUpperCase() + includesHyphen[0].slice(1) + '-' + includesHyphen[1][0].toUpperCase() + includesHyphen[1].slice(1);
+      hyphenArray.push(includesHyphen);
+    } else {
+      hyphenArray.push(titleArray[x]);
     }
-    return word[0].toUpperCase() + word.slice(1).toLowerCase();
-  }).join(' ');
+  }
+  for (var z = 0; z < hyphenArray.length; z++) {
+    if (hyphenArray[z] === 'Javascript:') {
+      output.push('JavaScript:');
+    } else if (hyphenArray[z] === 'api:') {
+      output.push('API:');
+    } else {
+      output.push(hyphenArray[z]);
+    }
+  }
+  output = output.join(' ');
+  return output;
 }
