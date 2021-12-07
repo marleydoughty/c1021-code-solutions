5b48677f8b4c7db79e6818dca779f84277b1e7c2
Marley Doughty <marleydoughty@yahoo.com>
2021-12-07 23:30:42 +0000

    compelted array-manipulation-3

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,4 +1,24 @@
 /* exported difference */
-function difference(first, second) {
 
+function checkIfIncludes(array, element) {
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] === element) {
+      return true;
+    }
+  }
+  return false;
+}
+function difference(first, second) {
+  const output = [];
+  for (let i = 0; i < first.length; i++) {
+    if (!checkIfIncludes(second, first[i])) {
+      output.push(first[i]);
+    }
+  }
+  for (let j = 0; j < second.length; j++) {
+    if (!checkIfIncludes(first, second[j])) {
+      output.push(second[j]);
+    }
+  }
+  return output;
 }
