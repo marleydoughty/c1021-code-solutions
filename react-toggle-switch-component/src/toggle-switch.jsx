32477ac5102c3fcd5eb2c9bcd66a5eb484b7d032
Marley Doughty <marleydoughty@yahoo.com>
2021-12-12 02:08:58 +0000

    styled track,ball and divs containing switch

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 class ToggleSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({ isClicked: false });
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
   render() {
-    return 'test';
+    return (
+      <div className='flex'>
+        <div className='toggleOuter'>
+            <div className='toggleTrack on'></div>
+            <div className='toggleBallOn'></div>
+        </div>
+        <div>ON</div>
+      </div>
+    );
   }
 }
-
 export default ToggleSwitch;
