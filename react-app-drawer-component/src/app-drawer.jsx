2a1ef81dee20dd560f17c4bf799535a1490b3f1e
Marley Doughty <marleydoughty@yahoo.com>
2021-12-14 20:07:25 +0000

    completed react-app-drawer-component

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -3,35 +3,40 @@ import React from 'react';
 class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ isOpen: false });
-    this.handleOnClick = this.handleOnClick.bind(this);
+    this.state = ({ showMenu: false });
+    this.handleOpenMenu = this.handleOpenMenu.bind(this);
+    this.handleMenuItems = this.handleMenuItems.bind(this);
   }
 
-  handleOnClick() {
-    this.setState({ isOpen: !this.state.isOpen });
+  handleOpenMenu() {
+    this.setState({ showMenu: true });
   }
 
-  handleMenuSlide() {
-    this.setState({ isOpen: true });
+  handleMenuItems() {
+    this.setState({ showMenu: false });
+  }
+
+  menuHidden() {
+    return (this.state.showMenu ? '' : 'hidden');
   }
 
   render() {
-    const menuBar = <i className='fas fa-bars'></i>;
+    const hidden = this.menuHidden();
     return (
-      <>
-      <div className='menu-bar'>
-        {menuBar}
-      </div>
       <div>
-        <nav className='slide-drawer hidden'>
-          <h2>MENU</h2>
-          <a>About</a>
-          <a>Get Started</a>
-          <a>Sign in</a>
-          <a>Sign up</a>
-        </nav>
+        <div className='menu-bar' onClick={this.handleOpenMenu}>
+          <i className='fas fa-bars'></i>
+        </div>
+        <div onClick={this.handleMenuItems} className={`drawer-outer ${hidden}`}>
+          <nav className='drawer-content'>
+            <h2>Choose a Game</h2>
+            <a onClick={this.handleMenuItems} href="#">Minecraft</a>
+            <a onClick={this.handleMenuItems} href="#">Fortnite</a>
+            <a onClick={this.handleMenuItems} href="#">Pac-Man</a>
+            <a onClick={this.handleMenuItems} href="#">Tetris</a>
+          </nav>
+        </div>
       </div>
-      </>
     );
   }
 }
