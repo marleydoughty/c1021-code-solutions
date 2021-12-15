62a5d4d954c05ccf2a799f24c24799740a068d90
Marley Doughty <marleydoughty@yahoo.com>
2021-12-15 19:07:35 +0000

    put path.join() return in a variable and added to static args

--- express-static/index.js
+++ express-static/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const staticFiles = express.static('public');
 
-path.join(__dirname, 'public');
+const pathJoin = path.join(__dirname, 'public');
+const staticFiles = express.static(pathJoin);
 
 app.use(staticFiles);
 
