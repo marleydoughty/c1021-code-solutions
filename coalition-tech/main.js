f7affb840dd7c7c0abf22a1ea591c138db633514
Marley Doughty <marleydoughty@yahoo.com>
2022-01-28 22:51:25 +0000

    working on second page

---
+++ coalition-tech/main.js
@@ -0,0 +1,20 @@
+// var $buttons = document.querySelectorAll('[data-link]');
+// var $tabView = document.querySelectorAll('[data-view]');
+
+// function activeView(event) {
+//   var viewContent = event.target.getAttribute('data-link');
+//   changeView(viewContent);
+
+// }
+// function changeView(viewContent) {
+//   for (var vi = 0; vi < $tabView.length; vi++) {
+//     if ($tabView[vi].getAttribute('data-view') === viewContent) {
+//       $tabView[vi].className = 'viewTab';
+//     } else {
+//       $tabView[vi].className = 'viewTab hidden';
+//     }
+//   }
+// }
+// for (var i = 0; i < $buttons.length; i++) {
+//   $buttons[i].addEventListener('click', activeView);
+// }
