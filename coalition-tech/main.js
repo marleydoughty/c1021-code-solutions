1b673832ebca0118a56cb95bb684fc1f7c75e5e5
Marley Doughty <marleydoughty@yahoo.com>
2022-01-29 02:48:14 +0000

    added view swapping function

--- coalition-tech/main.js
+++ coalition-tech/main.js
@@ -1,20 +1,20 @@
-// var $buttons = document.querySelectorAll('[data-link]');
-// var $tabView = document.querySelectorAll('[data-view]');
+var $buttons = document.querySelectorAll('[data-link]');
+var $tabView = document.querySelectorAll('[data-view]');
 
-// function activeView(event) {
-//   var viewContent = event.target.getAttribute('data-link');
-//   changeView(viewContent);
+function activeView(event) {
+  var viewContent = event.target.getAttribute('data-link');
+  changeView(viewContent);
 
-// }
-// function changeView(viewContent) {
-//   for (var vi = 0; vi < $tabView.length; vi++) {
-//     if ($tabView[vi].getAttribute('data-view') === viewContent) {
-//       $tabView[vi].className = 'viewTab';
-//     } else {
-//       $tabView[vi].className = 'viewTab hidden';
-//     }
-//   }
-// }
-// for (var i = 0; i < $buttons.length; i++) {
-//   $buttons[i].addEventListener('click', activeView);
-// }
+}
+function changeView(viewContent) {
+  for (var i = 0; i < $tabView.length; i++) {
+    if ($tabView[i].getAttribute('data-view') !== viewContent) {
+      $tabView[i].classList.add('hidden');
+    } else {
+      $tabView[i].classList.remove('hidden');
+    }
+  }
+}
+for (var i = 0; i < $buttons.length; i++) {
+  $buttons[i].addEventListener('click', activeView);
+}
